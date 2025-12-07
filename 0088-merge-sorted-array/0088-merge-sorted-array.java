3b3228732a2922b5a40a377c39c51b15391cb828
Varshini Mane <154003276+varshinimane@users.noreply.github.com>
2025-12-07 21:34:43 +0530

    Time: 0 ms (100%), Space: 43.6 MB (41.23%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -3,6 +3,7 @@ class Solution {
         int i = m - 1;
         int j = n - 1;
         int k = m + n - 1;
+
         while(i >= 0 && j >= 0){
             if(nums1[i] > nums2[j]){
                 nums1[k] = nums1[i];
@@ -10,7 +11,7 @@ class Solution {
             } else {
                 nums1[k] = nums2[j];
                 j--;
-            } 
+            }
             k--;
         }
         while(j >= 0){
