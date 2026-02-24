946eb9cc4302e1222ef0090e1cff309cc1cb6554
Varshini Mane <154003276+varshinimane@users.noreply.github.com>
2026-02-24 11:21:19 +0530

    Time: 4 ms (23.87%), Space: 43 MB (71.69%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -4,9 +4,6 @@ class Solution {
         for(int i = 0;i < sentence.length();i++){
             set.add(sentence.charAt(i));
         }
-        if(set.size() == 26){
-            return true;
-        }
-        return false;
+        return set.size() == 26;
     }
 }
