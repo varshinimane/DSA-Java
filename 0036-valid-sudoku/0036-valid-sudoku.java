6173b4475c2c454b467c75eec88d128138e9c568
Varshini Mane <154003276+varshinimane@users.noreply.github.com>
2026-02-26 11:06:31 +0530

    Time: 3 ms (50.94%), Space: 46.5 MB (68.64%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<Character>[] rows = new HashSet[9];
+        Set<Character>[] row = new HashSet[9];
         Set<Character>[] cols = new HashSet[9];
         Set<Character>[] boxes = new HashSet[9];
 
         for(int i = 0;i < 9;i++){
-            rows[i] = new HashSet<>();
+            row[i] = new HashSet<>();
             cols[i] = new HashSet<>();
             boxes[i] = new HashSet<>();
         }
@@ -13,14 +13,14 @@ class Solution {
         for(int i = 0;i < 9;i++){
             for(int j = 0;j < 9;j++){
                 char c = board[i][j];
+
                 if(c == '.') continue;
 
                 int boxidx = (i / 3) * 3 + (j / 3);
-
-                if(rows[i].contains(c) || cols[j].contains(c) || boxes[boxidx].contains(c)){
+                if(row[i].contains(c) || cols[j].contains(c) || boxes[boxidx].contains(c)){
                     return false;
                 }
-                rows[i].add(c);
+                row[i].add(c);
                 cols[j].add(c);
                 boxes[boxidx].add(c);
             }
