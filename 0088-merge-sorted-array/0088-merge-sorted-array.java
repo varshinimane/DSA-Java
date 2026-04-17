225722f898289638bc8040cbab8dde24b48a418c
Varshini Mane <154003276+varshinimane@users.noreply.github.com>
2026-04-17 21:52:38 +0530

    Time: 0 ms (100%), Space: 43.4 MB (95.25%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -7,7 +7,7 @@ class Solution {
             if(nums1[i] > nums2[j]){
                 nums1[k] = nums1[i];
                 i--;
-            }else {
+            } else {
                 nums1[k] = nums2[j];
                 j--;
             }
